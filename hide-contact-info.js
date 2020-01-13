23a487db3e65c967e8daf20e735367516e90b5e0
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-01-14 08:33:31 +1100

    Create hide-contact-info.js

---
+++ hide-contact-info.js
@@ -0,0 +1,32 @@
+
+var targets = [
+// phone numbers
+    /[+-\/\.\)\(\d\s]{7,22}/,
+// emails
+    /([-\w\d\.]+?)(\s+\(?at\)?\s+|\s*@\s*|\s*([\[\]@]){3}\s*)([-\w\d\.]*?)\s*(dot|\.|\[dot\]|\[.\])\s*(\w+)/,
+// skype usernames
+    /skype([\s-]?username)?\s?(:|at|with)?\s?\"?\S{3,16}\"?/i
+];
+
+replacement_text = '[contact information hidden]'
+
+var tid = setInterval( function () {
+
+    var paragraphs = document.getElementsByTagName('p')
+
+    for (var i = 0; i < paragraphs.length; i++) {
+
+        for (var j = 0; j < targets.length; j++) {
+            paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(targets[j], replacement_text);
+        }
+    }
+    var paragraphs1 = document.getElementsByClassName("conversation-title-link");
+
+    for (var i = 0; i < paragraphs1.length; i++) {
+        for (var j = 0; j < targets.length; j++) {
+            paragraphs1[i].innerHTML = paragraphs1[i].innerHTML.replace(targets[j], replacement_text);
+        }
+    }
+    if ( document.readyState !== 'complete' && (paragraphs.length < 1 || paragraphs1.length < 1)) return;
+    clearInterval( tid );
+}, 1 );

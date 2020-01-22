2e3b95119336254a5ef8abfd57c4285b13d3bd8f
demo-user-dev <60172409+demo-user-dev@users.noreply.github.com>
2020-01-22 18:38:33 +1100

    Create zendesk.js

---
+++ zendesk.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    changeZenDeskIcon();
+
+    function changeZenDeskIcon(){
+        var interval = setInterval(checkAndChangeIcon, 500);
+
+        function checkAndChangeIcon() {
+            if($("#launcher").length){
+                if($("#launcher").contents().find("#Embed .Icon").length){
+                    $("#launcher").contents().find("#Embed .Icon").html("<img class='chat-icon' src='https://raw.githubusercontent.com/bipashant/getapopup/master/logo-hd-25x25.png'/> ")
+                }
+                if($("#launcher").contents().find("#Embed .Icon--chat").length){
+                    $("#launcher").contents().find("#Embed .Icon--chat").html("<img class='chat-icon' src='https://raw.githubusercontent.com/bipashant/getapopup/master/logo-hd-25x25.png'/> ")
+                }
+                clearInterval(interval);
+                $("#launcher").contents().find("body").append("<style>.wrapperMobile-1Ets2 {padding: 0.75rem !important;}.wrapper-AtBcr{padding: 0.80857rem 1.57143rem;}.u-textInheritColor, .label-3kk12{color: #fff !important;}</style>");
+            }
+
+        }
+    }
+});

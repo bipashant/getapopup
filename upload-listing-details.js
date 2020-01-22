1fad7a6fed98f572d00f574d98ede730c21d6070
demo-user-dev <60172409+demo-user-dev@users.noreply.github.com>
2020-01-22 18:31:45 +1100

    Create upload-listing-details.js

---
+++ upload-listing-details.js
@@ -0,0 +1,74 @@
+
+$(document).ready(function(){
+    // Adding listing details to the https://api.myjson.com/bins/ihyt6
+
+
+    // Deleting Listing author from listing details page
+    if($(".listing-details-container").length){
+
+        $(".col-4 .listing-author").parents(".row-with-divider").remove()
+
+        existingData = {};
+        $.get("https://api.myjson.com/bins/ihyt6", function(data, textStatus, jqXHR) {
+            existingData = data;
+        });
+
+        var listingDetailstimer = setInterval(uploadListingDetails, 1000);
+
+        function uploadListingDetails(){
+
+            if(JSON.stringify(existingData) != "{}"){
+                clearInterval(listingDetailstimer);
+                $.ajax({
+                    url:"https://api.myjson.com/bins/ihyt6",
+                    type:"PUT",
+                    data:JSON.stringify(currentListingData()),
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data, textStatus, jqXHR){
+                        console.log("Data Uploaded")
+                    }
+                });
+            }
+
+        }
+
+
+        function currentListingData(){
+            existingData[listingId()] = {
+                listingImages: listingImages(),
+                location: location(),
+                squareFoot: squareFoot()
+            }
+            return existingData;
+        }
+
+        function listingId() {
+            return($("#listing_id").val());
+        }
+
+        function listingImages(){
+            var listingImages = [];
+            $(document).find(".listing-image-thumbnail").each(function(){
+                listingImages.push($(this).attr("src").replace('/thumb/', '/medium/'));
+            });
+            return listingImages;
+        }
+
+        function location(){
+            return($(document).find("#origin_loc_google_address").val().trim().split(", ").reverse()[2]);
+        }
+
+        function squareFoot(){
+            var sqFootLabel = "What is the space size? (in sq. ft.):";
+            var sqFootValue = '';
+            $("b").each(function () {
+                if($(this).text() == sqFootLabel){
+                    sqFootValue = $(this).parent().html().replace('<b>'+ sqFootLabel + '</b>', '').trim()
+                }
+            });
+
+            return sqFootValue;
+        }
+    }
+});

50b8c673762755bc5559faeb9f32c45f016a5ebd
demo-user-dev <60172409+demo-user-dev@users.noreply.github.com>
2020-01-22 18:32:10 +1100

    Create instagram.js

---
+++ instagram.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    if($("#hero__hero__0").length) {
+        var instaImages = [
+            {
+                image: 'https://6df2b785.ngrok.io/getapopup/images/1.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://6df2b785.ngrok.io/getapopup/images/2.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://6df2b785.ngrok.io/getapopup/images/3.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://6df2b785.ngrok.io/getapopup/images/4.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            }
+        ];
+
+        instagramContent = "<section class='instagram-feeds listings__section listings__section--zebra' style=' '> <div class='listings__content'> <h1 class='listings__title'>Follow us on Instagram</h1> <div class='listings__listings'>";
+        $(instaImages).each(function () {
+            instagramContent += "<div class='instagram-feeds-images listings__listing'> <a href='" + this.url + "' target='_blank'> <div class='listings__listing-image'style='background-image: url(" + this.image + ")'></div><div style='width:100%;height:100%;' class='instafeed-overlay '></div></a></div>"
+        });
+
+        $(instagramContent).insertBefore($("footer"));
+
+        $(document).on("mouseenter", ".instagram-feeds-images", function (e) {
+            $(this).find(".instafeed-overlay").css('opacity', 1)
+        });
+
+        $(document).on("mouseleave", ".instagram-feeds-images", function (e) {
+            $(this).find(".instafeed-overlay").css('opacity', 0)
+        });
+    };
+
+});

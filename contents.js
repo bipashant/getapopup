d01c2de6f3fc5497f32478d3505a8da8b93330e8
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-02-02 19:22:30 +1100

    Create contents.js

---
+++ contents.js
@@ -0,0 +1,24 @@
+
+
+var loginPageImageURL = "https://raw.githubusercontent.com/bipashant/getapopup/master/1.jpg";
+var signPageImageURL = "https://raw.githubusercontent.com/bipashant/getapopup/master/2.jpg";
+var feedbackPageImageURL = "https://raw.githubusercontent.com/bipashant/getapopup/master/3.jpg";
+
+var instaImages = [
+            {
+                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/1.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/2.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/3.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            },
+            {
+                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/4.jpg',
+                url: 'https://www.instagram.com/getapopup/'
+            }
+        ];

8c602fe25ec2a7e162c913bc3331b22b790630a1
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-02-02 19:22:19 +1100

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -1,24 +1,6 @@
 $(document).ready(function(){
     if($(".home-fluid-thumbnail-grid-narrow").length || $(".home-listings").length || $('.home-map').length) {
-        var instaImages = [
-            {
-                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/1.jpg',
-                url: 'https://www.instagram.com/getapopup/'
-            },
-            {
-                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/2.jpg',
-                url: 'https://www.instagram.com/getapopup/'
-            },
-            {
-                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/3.jpg',
-                url: 'https://www.instagram.com/getapopup/'
-            },
-            {
-                image: 'https://raw.githubusercontent.com/bipashant/getapopup/master/4.jpg',
-                url: 'https://www.instagram.com/getapopup/'
-            }
-        ];
-
+        
         instagramContent = "<section class='instagram-feeds listings__section listings__section--zebra' style=' '> <div class='listings__content'> <h1 class='listings__title'>Follow us on Instagram</h1> <div class='listings__listings'>";
         $(instaImages).each(function () {
             instagramContent += "<div class='instagram-feeds-images listings__listing'> <a href='" + this.url + "' target='_blank'> <div class='listings__listing-image'style='background-image: url(" + this.image + ")'></div><div style='width:100%;height:100%;' class='instafeed-overlay '></div></a></div>"

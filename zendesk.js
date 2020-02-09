c9b175f59269d465d9efc7cfa6598d9b19a71abc
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-02-09 11:55:23 +1100

    Update zendesk.js

--- zendesk.js
+++ zendesk.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
                 if($("#launcher").contents().find("#Embed .Icon--chat").length){
                     $("#launcher").contents().find("#Embed .Icon--chat").html("<img class='chat-icon' src='https://raw.githubusercontent.com/bipashant/getapopup/master/logo-hd-25x25.png'/> ")
                 }
-                $("#Embed span.label-3kk12").textContent = "Chat";
+                $("#launcher").contents().find(".label-3kk12").text("Chat")
                 $("#launcher").contents().find("body").append("<style>.wrapperMobile-1Ets2 {padding: 0.75rem !important;}.wrapper-AtBcr{padding: 0.80857rem 1.57143rem;}.u-textInheritColor, .label-3kk12{color: #fff !important;}</style>");
             }
 

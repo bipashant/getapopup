21e340f67e208ecf71cfac4eacb967f3205cec14
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-03-03 07:54:09 +1100

    Update filter-content.js

--- filter-content.js
+++ filter-content.js
@@ -40,9 +40,10 @@ $(document).ready(function(){
             $("#message").val(userContent);
         });
     }
-    setTimeout(function(){ 
-      $("a[href$='/infos/about']").remove();
-      $("a[href$='/infos/how_to_use']").remove();
-    }, 1000);
+    
+    if($("#hero__hero__0").length == 0){
+        // loading landing page css only in the marketplace. it is used to show the instagram thumbnail on the grid page view.
+        $("head").append('<link href="https://rawcdn.githack.com/bipashant/getapopup/3682e83/landing-page.css" rel="stylesheet"/>');
+    }
     
 });

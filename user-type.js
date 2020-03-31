fb75f413c2247dfeee3986689d7d997d76a9ae36
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-03-31 13:11:15 +1100

    Update user-type.js

--- user-type.js
+++ user-type.js
@@ -1,48 +1,113 @@
-var commonEmailServices = ['@gmail', '@hotmail', '@yahoo', '@aol', '@me', '@icloud'];
-
 $(document).ready(function(){
-  if($("#new_person").length){
-      moveUserTypeToTop();
-      addValidation();
+    if($("#new_person").length){
+        moveUserTypeToTop();
 
-      function moveUserTypeToTop(){
-        $("#person_custom_fields_0").prependTo("#new_person");
-        $("label[for='person_custom_fields_0']").parent().prependTo("#new_person");
+        hideWebsiteField();
+        wrapWebsiteInDiv();
 
-          $("#person_custom_fields_0").change(function(){
-              $( "#person_email" ).focus();
-              $( "#person_custom_fields_0" ).focus();
-              changeLabel();
-          });
-      }
+        initializeEmailRule();
+
+        function moveUserTypeToTop(){
+            var startElem = $(".signup-password").last();
+            var endElem   = $("label[for='person_custom_fields_1']").parent().last();
+            startElem.nextUntil(endElem).wrapAll("<div class='user-type-fields'></div>");
+            $('.user-type-fields').insertBefore("label[for='person_email']");
+
+            $("#person_custom_fields_0").change(function(){
+                $("#person_email").focus();
+                $("#person_custom_fields_0").focus();
+                changeLabel();
+            });
+        }
+
+        function wrapWebsiteInDiv(){
+            $("#person_custom_fields_1").attr("placeholder", "yourcompany.com");
+
+            var startElem = $(".signup-password").last();
+            var endElem   = $(".inline-label-container").last();
+            startElem.nextUntil(endElem).wrapAll("<div class='website-fields'></div>");
+            $(".website-fields").insertBefore("label[for='person_email']");
+        }
+
+        function hideWebsiteField(){
+            $("#person_custom_fields_1").hide();
+            $("label[for='person_custom_fields_1']").parent().hide();
+            removeRequiredValidationOnWebsite();
+            removeValidationOnEmail()
+        }
+
+        function showWebsiteField(){
+            $("#person_custom_fields_1").show();
+            $("label[for='person_custom_fields_1']").parent().show();
+            addRequiredValidationOnWebsite();
+            addValidationOnEmail();
+        }
+
+        function changeLabel(){
+            if($("#person_custom_fields_0 option:selected").text() == "Brand"){
+                $("label[for='person_given_name']").text("Brand Name");
+                $("#person_given_name").attr("placeholder", "ABC Group");
+                showWebsiteField();
+            }else{
+                $("label[for='person_given_name']").text("Full Name");
+                $("#person_given_name").attr("placeholder", "Alan E. Scott");
+                hideWebsiteField();
+            }
+        }
+
+        function addValidationOnEmail(){
+            $("#person_email").rules("add", { companyEmail: true });
+        }
+        function removeValidationOnEmail(){
+            $("#person_email").rules("remove", 'companyEmail');
+        }
+        function removeRequiredValidationOnWebsite(){
+            $('#person_custom_fields_1').rules('add',  { required: false });
+            $('#person_custom_fields_1').val("N/A");
+        }
+        function addRequiredValidationOnWebsite(){
+            $('#person_custom_fields_1').rules('add',  { required: true });
+            $('#person_custom_fields_1').val("");
+        }
+
+        function getDomain(website){
+            if(website.indexOf("https://") >= 0 ){
+                website = website.replace("https://", '');
+            }
+
+            if(website.indexOf("http://") >= 0){
+                website = website.replace("http://", '');
+            }
+
+            if(website.indexOf("www.") >= 0){
+                website = website.replace("www.", '');
+            }
 
 
-      function changeLabel(){
-          if($("#person_custom_fields_0 option:selected").text() == "Brand"){
-              $("label[for='person_given_name']").text("Brand Name")
-              $("#person_given_name").attr("placeholder", "ABC Group")
-          }else{
-              $("label[for='person_given_name']").text("Full Name")
-              $("#person_given_name").attr("placeholder", "Alan E. Scott")
-          }
-      }
+            domain = new URL('http://' + website).hostname;
+            return domain;
+        }
+        function initializeEmailRule(){
+            jQuery.validator.addMethod("companyEmail", function(value, element, params) {
+                var valid = true;
+                if($("#person_custom_fields_0 option:selected").text() == "Brand"){
+                    var userEmail = $("#person_email").val();
 
-      function addValidation(){
+                    var website   = $("#person_custom_fields_1").val();
 
-          jQuery.validator.addMethod("companyEmail", function(value, element, params) {
-              var valid = true;
-              if($("#person_custom_fields_0 option:selected").text() == "Brand"){
-                  var userEmail = $("#person_email").val();
-                  $(params).each(function(){
-                      if(userEmail.indexOf(this.toString()) > 0){
-                          valid = false;
-                      }
-                  })
-              }
-              return valid;
-          }, jQuery.validator.format("As a brand you should use the company email."));
+                    if(website.length){
+                        var hostname = getDomain(website);
+                        hostname = hostname.replace('www.','');
+                        if(userEmail.indexOf('@' + hostname) > 0){
+                            valid = true;
+                        }else{
+                            valid = false;
+                        }
+                    }
 
-          $("#person_email").rules("add", { companyEmail: commonEmailServices });
-      }
-  }
+                }
+                return valid;
+            }, jQuery.validator.format("As a brand you should use the company email."));
+        }
+    }
 });

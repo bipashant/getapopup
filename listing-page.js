05f7a38d0bd981dec22ece4841cc5c3d805f11a1
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2021-05-22 13:38:49 +1000

    Update listing-page.js

--- listing-page.js
+++ listing-page.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     if($(".listing-details-container").length){
         $(".wrapper").css("background", "#eee");
-        //$(".listing-details-container .row").first().prependTo($(".listing-details-container").parent())
-        //$("<div class='row'><div id='shareBlock'></div></div>").appendTo($(".listing-details-container"))
+        $(".listing-details-container .row").first().prependTo($(".listing-details-container").parent())
+        $("<div class='row'><div id='shareBlock'></div></div>").appendTo($(".listing-details-container"))
        
         setInterval(function(){
             $(".listing-image-arrow-icon-container.right").click();

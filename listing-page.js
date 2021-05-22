9794e363e0eb8e3af6bcc0a9928a538c918ab122
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2021-05-22 15:52:19 +1000

    Update listing-page.js

--- listing-page.js
+++ listing-page.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-    if($(".listing-details-container").length){
-        $(".wrapper").css("background", "#eee");
-        $(".listing-details-container .row").first().prependTo($(".listing-details-container").parent())
-        $("<div class='row'><div id='shareBlock'></div></div>").appendTo($(".listing-details-container"))
-       
-        setInterval(function(){
-            $(".listing-image-arrow-icon-container.right").click();
-        }, 5000)
+  if($(".listing-details-container").length){
+      $('body').addClass('listing-details-page')
+      $(".wrapper").css("background", "#eee");
+      $(".listing-details-container").parent();
+      $(".listing-details-container .row").first().prependTo($(".listing-details-container").parent())
+      var title = $('title').text();
+      var url = window.location.href;
+      $(".listing-details-container").append('<div id="shareBlock"></div>');
+      $('#shareBlock').append('<button class="button" data-sharer="twitter" data-title="'+ title +'" data-hashtags="getapopup" data-url="'+ url +'">Share on Twitter</button>');
+      $('#shareBlock').append('<button class="button" data-sharer="facebook" data-title="'+ title +'" data-hashtags="getapopup" data-url="'+ url +'">Share on Facebook</button>');
+      $('#shareBlock').append('<button class="button" data-sharer="linkedin" data-url="'+ url+'">Share on Linkedin</button>');
+      $('#shareBlock').append('<button class="button" data-sharer="email" data-title="'+ title +'" data-url="'+ url +'" data-subject="Hey! Check out that URL" data-to="">Share via Email</button>');
+      $('#shareBlock').append('<button class="button" data-sharer="whatsapp" data-title="'+ title +'" data-url="'+ url +'">Share on Whatsapp</button>');
 
-    }
+      window.Sharer.init();
+      setInterval(function(){
+          $(".listing-image-arrow-icon-container.right").click();
+      }, 5000)
+  }
 });

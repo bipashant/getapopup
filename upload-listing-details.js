25bb1f4df06289b2d1d78c6f19c032ce1e98a42d
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2021-05-22 20:37:18 +1000

    Update upload-listing-details.js

--- upload-listing-details.js
+++ upload-listing-details.js
@@ -1,77 +1,78 @@
 
 $(document).ready(function(){
-    // Adding listing details to the https://extendsclass.com/api/json-storage/bin/eebdfad
-    var JSONBLOB_URL = 'https://extendsclass.com/api/json-storage/bin/eebdfad'
+  // Adding listing details to the https://json.extendsclass.com/bin/b237a7059dea
+  var JSONBLOB_URL = 'https://json.extendsclass.com/bin/b237a7059dea';
 
-    // Deleting Listing author from listing details page
-    if($(".listing-details-container").length){
+  // Deleting Listing author from listing details page
+  if($(".listing-details-container").length){
 
-        $(".col-4 .listing-author").parents(".row-with-divider").remove();
-        uploadListingDetails();
+      $(".col-4 .listing-author").parents(".row-with-divider").remove();
+      uploadListingDetails();
 
-        function uploadListingDetails(){
-            listingData = {};
-            listingData[listingId()] = currentListingData();
-            const request = new XMLHttpRequest();
-            request.open("PATCH", JSONBLOB_URL, true);
-            request.setRequestHeader("Content-Type", 'application/json');
-            request.setRequestHeader("Accept", 'application/json');
-            request.send(JSON.stringify(listingData));
-            request.onreadystatechange = function(){
-                if(request.readyState==4 && request.status==200){
-                    console.log("Data Uploaded");
-                }
-            };
-        }
+      function uploadListingDetails(){
+          listingData = {};
+          listingData[listingId()] = currentListingData();
+          const request = new XMLHttpRequest();
+          request.open("PATCH", JSONBLOB_URL, true);
+          request.setRequestHeader("Content-Type", 'application/json');
+          request.setRequestHeader("Accept", 'application/json');
+          request.send(JSON.stringify(listingData));
+          request.onreadystatechange = function(){
+              if(request.readyState==4 && request.status==200){
+                  console.log("Data Uploaded");
+              }
+          };
+      }
 
-        function currentListingData(){
-            return({
-                listingImages: listingImages(),
-                location: location(),
-                squareFoot: squareFoot()
-            })
-        }
+      function currentListingData(){
+          return({
+              listingImages: listingImages(),
+              location: location(),
+              squareFoot: squareFoot()
+          })
+      }
 
-        function listingId() {
-            var lId = fetchListingId();
-            return(lId);
-        }
+      function listingId() {
+          var lId = fetchListingId();
+          return(lId);
+      }
 
-        function listingImages(){
-            var listingImages = [];
-            $(document).find(".listing-image-thumbnail").each(function(){
-                listingImages.push($(this).attr("src").replace('/thumb/', '/medium/'));
-            });
-            return listingImages;
-        }
+      function listingImages(){
+          var listingImages = [];
+          $(document).find(".listing-image-thumbnail").each(function(){
+              listingImages.push($(this).attr("src").replace('https://user-assets.sharetribe.com/images/listing_images/images/', '').replace('/thumb/', '/medium/'));
+          });
+          return listingImages;
+      }
 
-        function location(){
-            var address = $(document).find("#origin_loc_google_address").val();
-            if(address){
-                return(address.trim().split(", ").reverse()[2]);
-            }            
-        }
+      function location(){
+          var address = $(document).find("#origin_loc_google_address").val() || $("#googlemap .map-link a").attr('href').split("?q=")[1].replaceAll("%2C", ",").replaceAll("+", " ");
+          if(address){
+              address = address.trim().split(", ").reverse();
+              return(address[2] || address[1] || address[0]);
+          }            
+      }
 
-        function squareFoot(){
-            var sqFootLabel = "What is the space size? (in sq. ft.):";
-            var sqFootValue = '';
-            $("b").each(function () {
-                if($(this).text() == sqFootLabel){
-                    sqFootValue = $(this).parent().html().replace('<b>'+ sqFootLabel + '</b>', '').trim()
-                }
-            });
+      function squareFoot(){
+          var sqFootLabel = "What is the space size? (in sq. ft.):";
+          var sqFootValue = '';
+          $("b").each(function () {
+              if($(this).text() == sqFootLabel){
+                  sqFootValue = $(this).parent().html().replace('<b>'+ sqFootLabel + '</b>', '').trim()
+              }
+          });
 
-            return sqFootValue;
-        }
+          return sqFootValue;
+      }
 
-        function fetchListingId(){
-            url = window.location.href;
-            var listingId = url.substring(
-                url.lastIndexOf("/listings/") + 1,
-                url.indexOf("-")
-            ).replace("listings/", '').trim();
+      function fetchListingId(){
+          url = window.location.href;
+          var listingId = url.substring(
+              url.lastIndexOf("/listings/") + 1,
+              url.indexOf("-")
+          ).replace("listings/", '').trim();
 
-            return listingId;
-        }
-    }
+          return listingId;
+      }
+  }
 });
